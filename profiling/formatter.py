84ee826219c7ea5299959c684f220a414b378924
Heungsub Lee <sub@nexon.co.kr>
2014-08-26 11:44:51 +0900

    implement FrozenStat

--- profiling/formatter.py
+++ profiling/formatter.py
@@ -14,26 +14,30 @@ __all__ = ['Formatter']
 
 class Formatter(object):
 
-    def __init__(self, profiler):
+    def __init__(self, stat):
         super(Formatter, self).__init__()
-        self.profiler = profiler
+        self.stat = stat
 
-    def format_stats(self, stat=None, order=by_total_time, depth=0, buf=None):
-        if stat is None:
-            stat = self.profiler.stat
-        if buf is None:
-            buf = []
-        for child_stat in stat.sorted(order):
-            indent = ' ' * (depth * 2)
-            line = indent + '- {0:.2%} {1:.6f}/{2:.6f} {3}'.format(
-                child_stat.total_time / self.profiler.stat.total_time,
-                child_stat.own_time, child_stat.total_time,
-                child_stat.name)
-            buf.append(line)
-            if depth > 3:
+    def format_stat(self, stat):
+        return '{0:.2%} {1:.6f}/{2:.6f} {3}'.format(
+            stat.total_time / self.stat.total_time, stat.own_time,
+            stat.total_time, stat.name)
+
+    def format_stats(self, order=by_total_time, max_depth=3,
+                     _stat=None, _depth=0, _buf=None):
+        if _stat is None:
+            _stat = self.stat
+        if _buf is None:
+            _buf = [self.format_stat(_stat)]
+        for stat in _stat.sorted(order):
+            indent = ' ' * (_depth * 2)
+            _buf.append('{0}- {1}'.format(indent, self.format_stat(stat)))
+            if _depth >= max_depth:
                 continue
-            self.format_stats(child_stat, order, depth + 1, buf)
-        return buf
+            self.format_stats(order, max_depth, stat, _depth + 1, _buf)
+        return _buf
 
-    def print_stats(self, stream=sys.stdout, stat=None, order=by_total_time):
-        stream.write('\n'.join(self.format_stats(stat, order)).encode('utf-8'))
+    def print_stats(self, stream=sys.stdout, order=by_total_time, max_depth=3):
+        stream.write('\n'.join(self.format_stats(order, max_depth)))
+        stream.write('\n')
+        stream.flush()
--- profiling/profiler.py
+++ profiling/profiler.py
@@ -28,7 +28,7 @@ class Profiler(object):
         if timer is None:
             timer = Timer()
         self.timer = timer
-        self.stat = Stat()
+        self.stat = RecordingStat()
         self.times = {}
 
     def start(self):
@@ -44,12 +44,13 @@ class Profiler(object):
     def _profile(self, frame, event, arg):
         time = self.timer()
         if event.startswith('c_'):
+            # ignore c_call, c_return, c_exception
             return
         # find the parent stat
         parent_stat = self.stat
-        for f in self._get_frame_stack(frame.f_back):
+        for f in self._frame_stack(frame.f_back):
             try:
-                parent_stat = parent_stat[f.f_code]
+                parent_stat = parent_stat.get_child(f.f_code)
             except KeyError:
                 parent_stat = self.stat
                 break
@@ -63,7 +64,7 @@ class Profiler(object):
         self.times[frame] = time
         code = frame.f_code
         if code not in parent_stat:
-            parent_stat.add_child(Stat(code))
+            parent_stat.add_child(code, RecordingStat(code))
 
     def _leaved(self, frame, time, parent_stat):
         try:
@@ -73,61 +74,49 @@ class Profiler(object):
             return
         stat.record(timespan)
 
-    def _get_frame_stack(self, frame):
+    def _frame_stack(self, frame):
         frame_stack = deque()
         while frame is not None:
             frame_stack.appendleft(frame)
             frame = frame.f_back
         return frame_stack
 
-    def _repr_frame(self, frame):
-        co = frame.f_code
-        return \
-            '{0}:{1} {2}'.format(co.co_filename, co.co_firstlineno, co.co_name)
-
 
 class Stat(object):
+    """Statistic."""
 
-    def __init__(self, code=None):
+    name = None
+    filename = None
+    lineno = None
+    count = 0
+    total_time = 0.0
+
+    def __init__(self, name=None, filename=None, lineno=None):
         super(Stat, self).__init__()
-        self.code = code
-        self.children = {}
-        self.count = 0
-        self.total_time = 0.
-
-    def get_child(self, code, default=None):
-        return self.children.get(code, default)
-
-    def add_child(self, stat):
-        self.children[stat.code] = stat
-
-    def record(self, timespan):
-        assert timespan >= 0
-        self.count += 1
-        self.total_time += timespan
+        self.name = name
+        self.filename = filename
+        self.lineno = lineno
 
     def __iter__(self):
-        return self.children.itervalues()
-
-    def __contains__(self, code):
-        return code in self.children
-
-    def __getitem__(self, code):
-        return self.children[code]
-
-    def sorted(self, order=by_total_time):
-        return sorted(self, key=order, reverse=True)
+        """Override it to walk child stats."""
+        return iter(())
 
     @property
-    def name(self):
-        co = self.code
-        if co.co_filename.startswith('<'):
-            return co.co_filename
-        filename = os.path.basename(co.co_filename)
-        name = '{0}:{1}'.format(filename, co.co_firstlineno)
-        if co.co_name != '<module>':
-            name = '{0} at {1}'.format(co.co_name, name)
-        return name
+    def regular_name(self):
+        if self.name is None:
+            return
+        elif self.filename.startswith('<'):
+            # e.g. <string>
+            return self.filename
+        filename = os.path.basename(self.filename)
+        regular_name = '{0}:{1}'.format(filename, self.lineno)
+        if self.name == '<module>':
+            # e.g. mycode.py:89
+            pass
+        else:
+            # e.g. myfunc at mycode.py:123
+            regular_name = '{0} at {1}'.format(self.name, regular_name)
+        return regular_name
 
     @property
     def own_time(self):
@@ -142,8 +131,66 @@ class Stat(object):
     def own_time_per_call(self):
         return self.own_time / self.count
 
+    def sorted(self, order=by_total_time):
+        return sorted(self, key=order, reverse=True)
+
+    def frozen(self):
+        """Gets this stat as frozen."""
+        return FrozenStat(self)
+
     def __repr__(self):
         class_name = type(self).__name__
-        fmt = '<{0} {1}, count={2} total_time={3:.6f} own_time={4:.6f}>'
-        return fmt.format(
-            class_name, self.name, self.count, self.total_time, self.own_time)
+        name_string = self.regular_name
+        name_string = '' if name_string is None else name_string + ', '
+        fmt = '<{0} {1}count={2} total_time={3:.6f} own_time={4:.6f}>'
+        return fmt.format(class_name, name_string, self.count,
+                          self.total_time, self.own_time)
+
+
+class RecordingStat(Stat):
+    """Recordig statistic measures execution time of a code."""
+
+    def __init__(self, code=None):
+        if code is None:
+            args = ()
+        else:
+            args = (code.co_name, code.co_filename, code.co_firstlineno)
+        super(RecordingStat, self).__init__(*args)
+        self.children = {}
+
+    def record(self, timespan):
+        assert timespan >= 0
+        self.count += 1
+        self.total_time += timespan
+
+    def get_child(self, code):
+        return self.children[code]
+
+    def add_child(self, code, stat):
+        self.children[code] = stat
+
+    def __iter__(self):
+        return self.children.itervalues()
+
+    def __getitem__(self, code):
+        return self.get_child(code)
+
+    def __contains__(self, code):
+        return code in self.children
+
+
+class FrozenStat(Stat):
+    """Picklable statistic."""
+
+    def __init__(self, stat):
+        super(FrozenStat, self).__init__(stat.name, stat.filename, stat.lineno)
+        self.count = stat.count
+        self.total_time = stat.total_time
+        self.children = map(type(self), stat)
+
+    def __iter__(self):
+        return iter(self.children)
+
+    def frozen(self):
+        """Already frozen."""
+        return self
