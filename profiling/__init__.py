c3c46e3f1502a1cc83ed0318ab10153e043a1014
Heungsub Lee <sub@subl.ee>
2014-09-06 20:11:30 +0900

    remove profiling.formatter

--- profiling/__init__.py
+++ profiling/__init__.py
@@ -5,9 +5,8 @@
 """
 from __future__ import absolute_import
 
-from .formatter import Formatter
 from .profiler import Profiler
 
 
 __version__ = '0.0.0.dev'
-__all__ = ['Profiler', 'Formatter']
+__all__ = ['Profiler']
