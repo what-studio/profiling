7c77930c9c4dbdea5c722b5b187d34eb02a93a6f
Heungsub Lee <sub@subl.ee>
2014-09-08 03:02:32 +0900

    run flake8 on Travis CI

--- profiling/sortkeys.py
+++ profiling/sortkeys.py
@@ -24,20 +24,35 @@ class SortKey(object):
         return cls(lambda stat: -self.func(stat))
 
 
-by_name = SortKey(lambda stat: stat.name)
-by_module = SortKey(lambda stat: stat.module)
-by_function = SortKey(lambda stat: (stat.module, stat.name))
-by_calls = SortKey(lambda stat: -stat.calls)
-by_total_time = SortKey(lambda stat: -stat.total_time)
-by_own_time = SortKey(lambda stat: (-stat.own_time, -stat.total_time))
-
-
 def _by_total_time_per_call(stat):
     return -stat.total_time_per_call if stat.calls else -stat.total_time
-by_total_time_per_call = SortKey(_by_total_time_per_call)
 
 
 def _by_own_time_per_call(stat):
     return (-stat.own_time_per_call if stat.calls else -stat.own_time,
             _by_total_time_per_call(stat))
+
+
+#: Sorting by name in ascending order.
+by_name = SortKey(lambda stat: stat.name)
+
+#: Sorting by module in ascending order.
+by_module = SortKey(lambda stat: stat.module)
+
+#: Sorting by module and name in ascending order.
+by_function = SortKey(lambda stat: (stat.module, stat.name))
+
+#: Sorting by number of calls in descending order.
+by_calls = SortKey(lambda stat: -stat.calls)
+
+#: Sorting by total elapsed time in descending order.
+by_total_time = SortKey(lambda stat: -stat.total_time)
+
+#: Sorting by own elapsed time in descending order.
+by_own_time = SortKey(lambda stat: (-stat.own_time, -stat.total_time))
+
+#: Sorting by total elapsed time per call in descending order.
+by_total_time_per_call = SortKey(_by_total_time_per_call)
+
+#: Sorting by own elapsed time per call in descending order.
 by_own_time_per_call = SortKey(_by_own_time_per_call)
