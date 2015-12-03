75afa81b680928da77683ac024acbc9e8dadcd3e
Heungsub Lee <sub@subl.ee>
2015-12-04 01:21:13 +0900

    ProfilingServer doesn't exclude threading code

--- profiling/remote/__init__.py
+++ profiling/remote/__init__.py
@@ -19,10 +19,8 @@ except ImportError:
     import pickle
 import socket
 import struct
-import sys
 
 from .. import __version__
-from ..utils import current_thread_id, frame_stack, main_thread_id
 
 
 __all__ = ['LOGGER', 'LOG', 'INTERVAL', 'PICKLE_PROTOCOL',
@@ -169,17 +167,11 @@ class ProfilingServer(object):
 
         """
         self._log_profiler_started()
-        on_child_thread = current_thread_id() != main_thread_id
-        if on_child_thread:
-            # to exclude statistics of profiler server thread.
-            excluding_code = frame_stack(sys._getframe())[0].f_code
         while self.clients:
             self.profiler.start()
             # should sleep.
             yield
             self.profiler.stop()
-            if on_child_thread:
-                self.profiler.exclude_code(excluding_code)
             result = self.profiler.result()
             data = pack_msg(RESULT, result,
                             pickle_protocol=self.pickle_protocol)
